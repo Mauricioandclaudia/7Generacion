271ddacecd5d74854dc4a6af230102ac21ac7a26
Mauricio_l <mauricio_lopezs@hotmail.com>
2020-03-04 00:13:47 -0600

    ready

---
+++ p8hellokoders/js/main.js
@@ -0,0 +1,57 @@
+/*var string = "una cadena de caracteres";
+
+var number = 4;
+
+var object = {
+    name: "some name",
+    age: 30,
+    phone: "5568957595"
+}
+
+var array = [
+    "koder a",
+    "koder b",
+    "koder c"
+]
+
+var numbersArray = [
+    1, 2, 3, 4, 5
+]
+
+var crazyArray = {
+    "a"
+    "some string",
+    6,
+    {
+        some: "object"
+    }
+}
+
+console.log("Hola koders")
+
+console.warn("Hola koders")
+
+console.error("Nooooo koders")
+
+
+
+
+
+
+
+var someNumbre = 27;
+HTMLFormControlsCollection.log("la suema de x y Y es iohual a", somenumber)*/
+
+
+
+var x = 9
+
+var y = 3
+
+console.log(x + y)
+
+console.log(x - y)
+
+console.log(x * y)
+
+console.log(x / y)
