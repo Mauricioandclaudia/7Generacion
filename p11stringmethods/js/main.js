46372116d842d94fd0bfe0399045e63149a35f5f
Mauricio_l <mauricio_lopezs@hotmail.com>
2020-03-06 19:28:15 -0600

    p11

---
+++ p11stringmethods/js/main.js
@@ -0,0 +1,59 @@
+//
+var complete_name
+
+var complete_name = prompt("Ingresa tu nombre Completo")
+complete_name = complete_name.replace(/ /g, "")
+console.log(`Tu nombre contiene un numero de caracteres =  ${complete_name.length}`);
+
+
+
+if (complete_name.length < 15) {
+    console.log("Tu nombre es muy corto");
+} else {
+    console.log("Tu nombre es muy largo");
+}
+
+
+
+number_vocals = complete_name.match(/[aeiou]/gi).length
+
+console.log(number_vocals)
+
+
+
+
+//Algoritmo1
+
+//Declarar variable complete_name
+//asignar valor a complete_name mediante un prompt que de el mensaje "Ingresa tu nombre completo"
+//imprimir en la consola "Tu nombre contiene un numero de caracteres =  " y el numero de caracteres de complete_name utilizando el metodo complete_name.length
+//
+
+
+
+//Práctica 2
+
+//obtener el nombre completo del usuario
+//Contar cuántos caracteres tiene
+//si tu nombre tiene menos de 15 caracteres, indicarle mediante un mensaje que su nombre es mmuy corto
+//si tiene más de 15 caracteres indicarle que su nombre es muy largo
+
+
+//Algoritmo2
+//De la asignacion de la practica uno a complete_name verificar mediante un operador condicional si complete_name.length es menor a 15
+//Si es verdadera la condicion imprimir en consola "Tu nombre es muy corto"
+//En caso contrario imprimir en la consola "Tu nombre es muy largo"
+
+
+
+//Práctica 3
+
+//obtener el nombre completo del usuario
+//contar cuántas vocales tiene
+
+//Algoritmo3
+//crear una variable llamada number_vocals y asignar mediante el metodo .match entre parentesis indicando una barra diagonal y abriendo un corchete los caracteres a buscar que en este caso son las vocales aeiou, cerrando corchete y colocando nuevamente barra diagonal
+//delante de la barra diagonal de cierre indicamos la letra g para que busque  todas las coincidencias
+//delante de la g indicar la letra i para que no distinga entre mayusculas y minusculas
+//despues del parentesis de cierre agregar el metodo .length para indicar el numero de caracteres
+//imprimir la variable number_vocals
---
+++ p9hellokoders/js/js.js
@@ -0,0 +1,130 @@
+//tener los valores de x y de y  y compararlos
+
+//si x es mayor que y  mostrar un mensaje que lo indique 
+
+//si x es menor que y  moestrar un mensaje que lo indique
+
+
+//algoritmo
+
+//obtener los valores de x Y y
+
+//preguntar si x es mayor que y 
+
+//obtener la respues de la comparacion
+
+//si x es mayor que y  mostrar el mensaje "x es mayor y"
+
+//si x no es mayor que y  mostrar el mensaje "x es mayor que y"
+
+
+/*
+var y = 9
+var y = 3
+
+
+if (9 > 3) {
+
+    console.log("9 es mayor que 3");
+
+} else if (9 < 3) {
+
+    console.log("9 es menor que 3");
+
+} else {
+
+    console.log("9 si es igual 3")
+}
+
+*/
+
+/*
+debugger
+
+
+string interpolition */
+ 
+
+
+
+/*
+multiplicacion dos valores y verificar si el resultado de la multiplicaion es par o non
+si es par indicar con un mensaje
+
+
+var x = 9
+var y = 3
+
+
+var multiplicacion = 9 * 3;
+
+conlose.log("si el resulatdo es "){
+    
+}
+*/
+
+
+
+/*recibir un numero de parte del usuario y calcular el area de un circulo cuyo radio sea igual al numero
+
+
+var person = prompt("Please enter your name", "Harry Potter");
+
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+}
+
+
+
+
+
+metodo window investigar
+
+
+
+
+datos de un trompt
+
+algorimo y codigo*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/
+practica 4
+
+//recibir del usuario los datos de 3 lados de un triangulo
+//con base en esos datos, determinar el área del triangulo y mostrarla en un mensaje
+//en otro mensaje, indicar si el triangulo es isoseles, equilátero o escaleno
+//practica 5
+//una vez lograda la practica 4, determinar si el triángulo es un triángulo rectángulo
+
+
+
+var lado_A = promt("introduce el lado A");
+
+var lado_B = promt("introduce el lado B");
+
+var lado_C = promt("introduce el lado C");
+
+
+var string
