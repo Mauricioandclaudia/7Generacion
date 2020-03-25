5da91e2c8a9eb1de1fc4e28f665774735d545272
Mauricio_l <mauricio_lopezs@hotmail.com>
2020-03-25 12:32:35 -0600

    p19final

--- p15_constructors/js/main.js
+++ p15_constructors/js/main.js
@@ -368,4 +368,3 @@ const objectProps = (obj) => {
     console.log(obj)
 }
 objectProps(koder);
-}
--- p19/js/main.js
+++ p19/js/main.js
@@ -1,3 +1,140 @@
+const saveProductToDb = (productObject) => {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+        }
+    }
+    xhttp.open("POST", "https://javascript-ajax-d0ce6.firebaseio.com/israel/products.json", true)
+    xhttp.send(JSON.stringify(productObject))
+}
+const getProductData = () => {
+    let name = document.getElementById("product-name").value
+    let description = document.getElementById("product-description").value
+    let price = document.getElementById("product-price").value;
+    price = parseInt(price)
+    let productObject = { name, description, price }
+    console.log(productObject)
+    saveProductToDb(productObject)
+}
+let saveButton = document.getElementById("save-product")
+saveButton.addEventListener("click", getProductData)
+const getProducts = () => {
+    console.log("getting products")
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let response = JSON.parse(this.responseText)
+            console.log(response)
+        }
+    }
+    xhttp.open("GET", "www.israelsalinas.com", true)
+    xhttp.send()
+}
+const putProduct = (productId, productObject) => {
+    console.log("getting products")
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let response = JSON.parse(this.responseText)
+            console.log(response)
+        }
+    }
+    xhttp.open("PUT", `https://javascript-ajax-d0ce6.firebaseio.com/israel/products/${productId}.json`, true)
+    xhttp.send(productObject)
+}
+const deleteProduct = (productId) => {
+    console.log("getting products")
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let response = JSON.parse(this.responseText)
+            console.log(response)
+        }
+    }
+    xhttp.open("DELETE", `https://javascript-ajax-d0ce6.firebaseio.com/israel/koders/${productId}.json`, true)
+    xhttp.send()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+/*GET
+
+const errorfield = () => {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            productsArray = JSON.parse(this.responseText)
+            errorfield();
+        }
+    }
+    xhttp.open("GET", "https://javascript-ajax-d0ce6.firebaseio.com/israel/products.json", true)
+    xhttp.send();
+}
+
+
+
+
+/*POST
+const errorfieldBd = () => {
+    let xhttp = new XMLHttpRequest();
+    //objeto a guardar en la base//
+    let productObject = {
+        name: "post test"
+    }
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+        }
+    }
+
+    xhttp.open("POST", "https://javascript-ajax-d0ce6.firebaseio.com/israel/products.json", true)
+    xhttp.send(JSON.stringify(productObject))
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var productsArray = [{
         name: "Sabritas 340gr",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, laudantium.",
@@ -178,5 +315,4 @@ const deleteProductFromChart = (event) => {
 //CON ELSPLICE ES PARA AGREGAR O BORRAR UN ELEMENTO
 
 
-
-
+ */
