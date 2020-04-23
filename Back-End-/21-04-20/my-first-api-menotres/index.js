1b0a6124f7b51f76d382e574fd52ab50abf08893
Mauricio_l <mauricio_lopezs@hotmail.com>
2020-04-22 19:17:30 -0500

    updated

--- Back-End-/21-04-20/my-first-api-menotres/index.js
+++ Back-End-/21-04-20/my-first-api-menotres/index.js
@@ -1,71 +1,74 @@
-const fs = require('fs')
 const express = require('express')
 
-const kodemia = JSON.parse(fs.readFileSync('kodemia.json'))
-
 const server = express()
-server.use(express.json()) //Middleware
 
-//endpoints: la combinación de un método y una ruta
-// método: Métoos HTTP, GET, POST, DELETE, PUT, PATCH
-// ruta: strings que denotan recursos en nuestro servidor.
+const fs = require('fs')
+
+
+const kodemia = JSON.parse(fs.readFileSync('kodemia.json'))
+server.use(express.json())
+
+
 
 server.get('/', (request, response) => {
     response.json({
-        message: 'My first API is running'
+        message: 'My first API is runing'
     })
 })
 
-//Section koders 
-server.get('/koders', (request, response) => {
-    response.json({
-        message: 'All the koders',
-        data: {
-            koders: kodemia.koders
-        }
-    })
-})
 
+server.get('/Koders', (request, response) => {
+        response.json({
+            message: 'All the koders',
+            data: {
+                koders: kodemia.koders
+            }
+        })
+    })
+    //--------------------------------------------//
 server.post('/koders', (request, response) => {
     const newKoder = {
         name: request.body.name,
         generation: request.body.generation
     }
+
     kodemia.koders.push(newKoder)
+
     response.json({
-        message: 'new koder added',
+        message: 'New koder added',
         data: {
             koder: newKoder
         }
     })
 })
 
-//Sectio a-Mentors
-server.get('/amentores', (request, response) => {
+
+//------------------------------------------------------//
+server.get('/mentors', (request, response) => {
     response.json({
-        message: 'All the aMentores',
+        message: 'List of  mentors',
         data: {
-            aMentors: kodemia.aMentors
+            koders: kodemia.mentors
         }
     })
 })
 
-server.post('/amentores', (request, response) => {
-    const newMentor = {
-        name: request.body.name,
-        course: request.body.course,
-        generation: request.body.generation
-    }
-    kodemia.aMentores.push(newMentores)
-    response.json({
-        message: 'new aMentores aded',
-        data: {
-            aMentor: newMentores
+server.post('/mentors', (request, response) => {
+        const newMentor = {
+            name: request.body.name,
+            type: request.body.type
         }
+
+        kodemia.mentors.push(newMentor)
+
+        response.json({
+            message: 'New mentor ',
+            data: {
+                mentor: newMentor
+            }
+        })
     })
-
-})
-
+    //----------------------------------------------//
 server.listen(8081, () => {
     console.log('Server is running')
 })
