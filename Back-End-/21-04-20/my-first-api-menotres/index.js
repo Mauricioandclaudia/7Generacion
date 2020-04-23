28f1a5ddd28e9e50f0f6eb80caa40f6f48251fd9
Mauricio_l <mauricio_lopezs@hotmail.com>
2020-04-23 14:05:27 -0500

    updated

--- Back-End-/21-04-20/my-first-api-menotres/index.js
+++ Back-End-/21-04-20/my-first-api-menotres/index.js
@@ -1,52 +1,81 @@
-const express = require('express')
-
-const server = express()
-
 const fs = require('fs')
+const express = require('express')
+const server = express()
+server.use(express.json()) // use, introduce el concepto de middleware, se ejecuta cada vez que recibe una función, 
+    //parsea el cuerpo del paquete express ajson que es manejable
+
+const kodemia = JSON.parse(fs.readFileSync('my-first-api/kodemia.json'))
 
 
-const kodemia = JSON.parse(fs.readFileSync('kodemia.json'))
-server.use(express.json())
 
-
-
+//------------------------------------
+// Endpoint, la combinación de un metodo y una ruta
+// metodo: metodos HTTP - GET POST DELETE PUT PATCH
+//ruta: strings que denotan recursos en nuestroservidor, unicamente representa los recursos, no archios de texto u otros
 server.get('/', (request, response) => {
     response.json({
-        message: 'My first API is runing'
+        message: 'My first API is running'
     })
 })
 
 
-server.get('/Koders', (request, response) => {
-        response.json({
-            message: 'All the koders',
-            data: {
-                koders: kodemia.koders
-            }
-        })
-    })
-    //--------------------------------------------//
-server.post('/koders', (request, response) => {
-    const newKoder = {
-        name: request.body.name,
-        generation: request.body.generation
-    }
-
-    kodemia.koders.push(newKoder)
-
-    response.json({
-        message: 'New koder added',
+//json automaticamente termina la conexion no es necesario el response.end
+//-------------------------------------------
+server.get('/koders', (req, res) => {
+    //using query parameters
+    const { limit = 10 } = req.query
+    res.json({
+        message: 'All the koders',
         data: {
-            koder: newKoder
+            koders: kodemia.koders.slice(0, parseInt(limit))
         }
     })
 })
 
 
-//------------------------------------------------------//
-server.get('/mentors', (request, response) => {
+//--------------------------------------------------
+server.post('/koders', (request, response) => {
+    //usando deconstruccion
+    const { name, generation } = request.body
+
+    kodemia.koders.push({
+        name,
+        generation
+    })
+
     response.json({
-        message: 'List of  mentors',
+        message: 'new koder added',
+        data: {
+            koder: {
+                name,
+                generation
+            }
+        }
+    })
+
+    // response.json(request.body)
+    // const newKoder = {
+    //     name: request.body.name,
+    //     generation: request.body.generation
+    // }
+    // kodemia.koders.push(newKoder)
+
+    // response.json({
+    //     messagge: 'New koder add',
+    //     data: {
+    //         newKoder
+    //     }
+
+    // })
+})
+
+
+
+//---------------------------------------------------
+// POST and GET to mentors
+server.get('/mentors', (req, res) => {
+    res.json({
+        message: 'All the mentors',
         data: {
             koders: kodemia.mentors
         }
@@ -54,21 +83,57 @@ server.get('/mentors', (request, response) => {
 })
 
 server.post('/mentors', (request, response) => {
-        const newMentor = {
-            name: request.body.name,
-            type: request.body.type
+    const newMentor = {
+        name: request.body.name,
+        type: request.body.type
+    }
+    kodemia.mentors.push(newMentor)
+
+    response.json({
+        messagge: 'New mentor add',
+        data: {
+            newMentor
         }
+    })
+})
 
-        kodemia.mentors.push(newMentor)
+//---------------------------------------
+// uri parameters
+// GET /koders/[name]
+// GET /koders/fer -> name = fer
 
+server.get('/koders/:name', (request, response) => {
         response.json({
-            message: 'New mentor ',
+            name: request.params.name
+        })
+    }) // -> /koders/fer
+
+// query parameters
+// estan en una url http://servidor/ruta/del/recurso?
+// GET http://api.kodemia.mx/koders?limit=66&from=10&to=100 ->fin de un parametro y comienzo de otro
+
+// -------------------Practica---------------------
+/*
+Endpoint que reciba el indice de un koder y regrese el koder que se encuentra en ese indice
+GET /koders/:indice _> indice = 0 ... kodemia.koders[indice]
+*/
+
+
+//-------------------------------------
+server.get('/kodersIndex/:indice', (req, res) => {
+        //using query parameters
+        const { indice } = req.params
+        res.json({
+            message: 'The koder is...',
             data: {
-                mentor: newMentor
+                koder: kodemia.koders[indice].name
             }
         })
     })
-    //----------------------------------------------//
-server.listen(8081, () => {
+    //----------------------------------
+
+
+
+server.listen(8080, () => {
     console.log('Server is running')
 })
---
+++ Back-End-/21-04-20/practica-por-mi/server_for_my.js
@@ -0,0 +1,31 @@
+const express = require('express') //mandamos a llamar a express con una constante
+
+const server = express() //en esta linea estamos creando una nueva aplicacion en express
+
+
+
+//----------ha estos se les llama endpoints: la combinacion de un metodo y una ruta----------//
+//-----------los metodo: metoodos HTTP SON: -get,post,delete,put patch-------//
+//-----------las ruta: strings que denotan recursos en nuestro servidor------//
+
+server.get('/', (request, response) => { //el REQUEST es el objeto que nos manda el cliente y 
+        // el RESPONSE es es la respuesta del cliente
+        response.json({
+
+            message: 'My first api is running papi'
+
+        })
+
+    }) //quiero que este server me responda en un llamada GET a la raiz
+    //  (tiene dos paramentros de cajon que son: 1.-'/'  2.-)
+    // tambien recibe un callback    () => {}
+
+
+
+//como hago algo con este servidor ?????
+
+server.listen(8080, () => {
+        console.log('server is running')
+    }) //el 8080 es el primer paramentro de listen, falta otro que es :
+    //callback que se ejecuta cuando termina de levantar  el servidor del puerto  
+    //le vamos a decir al servidor donde va a escuchar y esto sera en el puerto 8080
